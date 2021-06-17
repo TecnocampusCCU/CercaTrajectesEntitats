cf674536db9dccac6d8b97a8a91367b167952fde
Manuel Duro <mduro@edu.tecnocampus.cat>
2021-06-17 10:30:15 +0200

    Q3.210617
    Arreglado bug versión QGIS3.10 Coruña

--- CercaTrajectesEntitats.py
+++ CercaTrajectesEntitats.py
@@ -84,7 +84,7 @@ from itertools import dropwhile
 Variables globals per a la connexio
 i per guardar el color dels botons
 """
-Versio_modul="V_Q3.210215"
+Versio_modul="V_Q3.210617"
 nomBD1=""
 contra1=""
 host1=""
@@ -1617,7 +1617,7 @@ class CercaTrajectesEntitats:
         if vlayer.isValid():
             Cobertura=datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
             """Es crea un Shape a la carpeta temporal amb la data i hora actual"""
-            if (qgis.utils.Qgis.QGIS_VERSION_INT>=31000):
+            if (qgis.utils.Qgis.QGIS_VERSION_INT>31000):
                 save_options = QgsVectorFileWriter.SaveVectorOptions()
                 save_options.driverName = "ESRI Shapefile"
                 save_options.fileEncoding = "UTF-8"
@@ -1910,7 +1910,7 @@ class CercaTrajectesEntitats:
         if vlayer.isValid():
             Cobertura=datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
             """Es crea un Shape a la carpeta temporal amb la data i hora actual"""
-            if (qgis.utils.Qgis.QGIS_VERSION_INT>=31000):
+            if (qgis.utils.Qgis.QGIS_VERSION_INT>31000):
                 save_options = QgsVectorFileWriter.SaveVectorOptions()
                 save_options.driverName = "ESRI Shapefile"
                 #save_options.fileEncoding = "UTF-8"
@@ -2559,7 +2559,7 @@ class CercaTrajectesEntitats:
         if vlayer.isValid():
             Cobertura=datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
             """Es crea un Shape a la carpeta temporal amb la data i hora actual"""
-            if (qgis.utils.Qgis.QGIS_VERSION_INT>=31000):
+            if (qgis.utils.Qgis.QGIS_VERSION_INT>31000):
                 save_options = QgsVectorFileWriter.SaveVectorOptions()
                 save_options.driverName = "ESRI Shapefile"
                 save_options.fileEncoding = "UTF-8"
@@ -2664,7 +2664,7 @@ class CercaTrajectesEntitats:
         if vlayer.isValid():
             Cobertura=datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
             """Es crea un Shape a la carpeta temporal amb la data i hora actual"""
-            if (qgis.utils.Qgis.QGIS_VERSION_INT>=31000):
+            if (qgis.utils.Qgis.QGIS_VERSION_INT>31000):
                 save_options = QgsVectorFileWriter.SaveVectorOptions()
                 save_options.driverName = "ESRI Shapefile"
                 save_options.fileEncoding = "UTF-8"
@@ -2744,7 +2744,7 @@ class CercaTrajectesEntitats:
         if vlayer.isValid():
             Cobertura=datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
             """Es crea un Shape a la carpeta temporal amb la data i hora actual"""
-            if (qgis.utils.Qgis.QGIS_VERSION_INT>=31000):
+            if (qgis.utils.Qgis.QGIS_VERSION_INT>31000):
                 save_options = QgsVectorFileWriter.SaveVectorOptions()
                 save_options.driverName = "ESRI Shapefile"
                 save_options.fileEncoding = "UTF-8"
